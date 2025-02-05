8c86d56d6b77585dc3092a6f0541a440d8e79368
Fabien Gaubert <fab.gaubert@gmail.com>
2025-02-05 18:12:07 +0100

    add parsing of ws packets

--- ws_client.c
+++ ws_client.c
@@ -5,12 +5,43 @@
 #include "lwip/pbuf.h"
 #include "lwip/tcp.h"
 
-typedef enum ws_state_t {
+
+    // From rfc6455
+    //
+    //   0                   1                   2                   3
+    //   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    //  +-+-+-+-+-------+-+-------------+-------------------------------+
+    //  |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+    //  |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+    //  |N|V|V|V|       |S|             |   (if payload len==126/127)   |
+    //  | |1|2|3|       |K|             |                               |
+    //  +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+    //  |     Extended payload length continued, if payload len == 127  |
+    //  + - - - - - - - - - - - - - - - +-------------------------------+
+    //  |                               |Masking-key, if MASK set to 1  |
+    //  +-------------------------------+-------------------------------+
+    //  | Masking-key (continued)       |          Payload Data         |
+    //  +-------------------------------- - - - - - - - - - - - - - - - +
+    //  :                     Payload Data continued ...                :
+    //  + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+    //  |                     Payload Data continued ...                |
+    //  +---------------------------------------------------------------+
+
+enum ws_op_code {
+    OP_CODE_CONTINUATION = 0x0,
+    OP_CODE_TEXT = 0x1,
+    OP_CODE_BINARY = 0x2,
+    OP_CODE_CONNECTION_CLOSE = 0x8,
+    OP_CODE_PING = 0x9,
+    OP_CODE_PONG = 0xA
+};
+
+enum ws_state {
     WS_DISCONNECTED,
     WS_CONNECTING,
     WS_HANDSHAKING,
     WS_CONNECTED
-} ws_state_t;
+};
 
 struct ws_client {
     struct tcp_pcb *pcb;
@@ -18,7 +49,7 @@ struct ws_client {
     uint16_t server_port;
     uint8_t send_buffer[BUF_SIZE];
     int send_buffer_len;
-    ws_state_t state;
+    enum ws_state state;
 };
 
 err_t ws_client_close(struct ws_client* client) {
@@ -96,7 +127,6 @@ static err_t ws_client_connected(void *arg, struct tcp_pcb *pcb, err_t err) {
 }
 
 static err_t ws_client_poll(void *arg, struct tcp_pcb *pcb) {
-    printf("Polling callback.\n");
     return ERR_OK;
 }
 
@@ -104,6 +134,69 @@ static void ws_client_err(void *arg, err_t err) {
     struct ws_client *client = (struct ws_client*) arg;
     ws_client_close(client);
     printf("Client error: %d\n", err);
+}
+
+static void ws_print_message(const unsigned char* buffer, uint32_t length) {
+    for (int index = 0; index < length; index++) {
+        printf("%c", buffer[index]);
+    }
+    printf("\n");
+}
+
+static void ws_parse_packet(const unsigned char* buffer, u16_t length) {
+    printf("Packet parser!\n");
+
+    /* Only messages sent by the client should be masked */
+    if (buffer[1] & 0x80) {
+        printf("Error: message coming from the server is masked!\n");
+    }
+    if (buffer[0] & 0x70) {
+        printf("Error: an extension must be managed!\n");
+    }
+
+    /* Payload length calculation */
+    uint32_t payload_length;
+    uint8_t payload_offset = 2;
+
+    payload_length = buffer[1] & 0x7F;
+
+    /* 126 and 127 indicate an extention of the payload length */
+    if (payload_length == 126) {
+        payload_length = (buffer[2] << 8) & buffer[3];
+        payload_offset += 2;
+    }
+    else if (payload_length == 127) {
+        payload_length = (buffer[2] << 24) & (buffer[3] << 16) & (buffer[4] << 8) & buffer[5];
+        payload_offset += 4;
+    }
+    printf("Length of the payload: %d\n", payload_length);
+
+    /* Op Code */
+    printf("OP CODE:\n");
+
+    switch (buffer[0] & 0x0F) {
+        case OP_CODE_CONTINUATION:
+            printf("Continuation of a packet.\n");
+            break;
+        case OP_CODE_TEXT:
+            ws_print_message(&buffer[payload_offset], payload_length);
+            break;
+        case OP_CODE_BINARY:
+            printf("Binary.\n");
+            break;
+        case OP_CODE_CONNECTION_CLOSE:
+            printf("Connection closed.\n");
+            break;
+        case OP_CODE_PING:
+            printf("We received a ping.\n");
+            break;
+        case OP_CODE_PONG:
+            printf("We received a pong.\n");
+            break;
+        default:
+            printf("Error: packet not managed.\n");
+    }
+    return;
 }
 
 err_t ws_client_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err) {
@@ -115,17 +208,25 @@ err_t ws_client_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err) 
         return err;
     }
 
-    if (client->state == WS_HANDSHAKING) {
+    if (client->state == WS_CONNECTED) {
+        int index = 0;
+        printf("Total packet length: %d\n", p->tot_len);
+        printf("Segment length: %d\n", p->len);
+        for (struct pbuf *q = p; q != NULL; q = q->next) {
+            ws_parse_packet((const unsigned char*)p->payload, p->len);
+        }
+    }
+    else if (client->state == WS_HANDSHAKING) {
         char *upgrade_string = strstr(p->payload, "Upgrade: websocket");
         if (upgrade_string != NULL) {
             client->state = WS_CONNECTED;
             printf("Handshake accepted.\n");
         }
     }
-
-    /* Print packet */
-    for (struct pbuf *q = p; q != NULL; q = q->next) {
-        printf("%.*s\n", q->len, (const char*)q->payload);
+    else {
+        printf("Incorrect packet received. Closing connection.\n");
+        ws_client_close(client);
+        return err;
     }
 
     tcp_recved(pcb, p->tot_len);
